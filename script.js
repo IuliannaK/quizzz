a927e41196bd8d9227943d60e62e6fbd2d486e66
Ase444ka <istomina.asia@yandex.ru>
2023-06-09 17:31:32 +0300

    fix formatting

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-const button = document.querySelector(".btn");
-button.addEventListener("click", result);
+const button = document.querySelector('.btn');
+button.addEventListener('click', result);
 
-const par = document.querySelector("#show");
+const par = document.querySelector('#show');
 
+function result(e) {
+  e.preventDefault();
 
-function result (e) {
-    e.preventDefault();
-    
-    let points = 0;
+  let points = 0;
 
-    if(document.querySelector("#answer1").checked) {
-        points++;
-    }
+  if (document.querySelector('#answer1').checked) {
+    points++;
+  }
 
-    if (document.querySelector("#answer2").checked) {
-        points++;
-        
-    }
+  if (document.querySelector('#answer2').checked) {
+    points++;
+  }
 
-    if (document.querySelector("#answer3").checked) {
-        points++;
-    }
+  if (document.querySelector('#answer3').checked) {
+    points++;
+  }
 
-    if (document.querySelector("#answer4").checked) {
-        points++;
-    }
+  if (document.querySelector('#answer4').checked) {
+    points++;
+  }
 
-    par.textContent = "Ваши баллы: " + points
-        
-    }
-
-   
-    
+  par.textContent = 'Ваши баллы: ' + points;
+}
